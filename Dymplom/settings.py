be73959727387becab8bd847a4670d260246c402
yaroshkin <yaroshkin255@ukr.net>
2023-03-07 20:12:19 +0200

    add image

--- Dymplom/settings.py
+++ Dymplom/settings.py
@@ -124,5 +124,5 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,"static","media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
