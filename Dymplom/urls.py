451687da5e68eb01e2eca5bb3d06d23588a3bf31
yaroshkin <yaroshkin255@ukr.net>
2023-03-22 01:29:53 +0200

    changed urls

--- Dymplom/urls.py
+++ Dymplom/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -29,3 +30,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+
+urlpatterns += staticfiles_urlpatterns()
