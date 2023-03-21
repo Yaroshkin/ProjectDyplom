9a5408f3d525cced334f9f399afc2b39791ac6d6
yaroshkin <yaroshkin255@ukr.net>
2023-03-22 01:34:44 +0200

    changed urls1

--- Dymplom/urls.py
+++ Dymplom/urls.py
@@ -25,10 +25,14 @@ urlpatterns = [
     path('',include("orders.urls")),
     path('', include('main.urls'))
 ] \
-        + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+        + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
+        + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+
 
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
 
-urlpatterns += staticfiles_urlpatterns()
+
