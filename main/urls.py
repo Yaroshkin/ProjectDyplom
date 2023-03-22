b19177480795dc0040334428b5013ebaf12314ed
yaroshkin <yaroshkin255@ukr.net>
2023-03-22 02:31:46 +0200

    changed urls

--- main/urls.py
+++ main/urls.py
@@ -4,10 +4,9 @@ from . import views
 urlpatterns = [
     path('',views.home),
     path('catalogs',views.catalogs),
-    path('cart', views.cart),
     path('about',views.about),
     path('telephone',views.phone),
     path('notebook',views.notebook),
     path('accesory',views.accesory),
-    path('register',views,main)
+    path('register',views)
 ]
